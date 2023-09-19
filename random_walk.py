ba2096db768c3ff9d4452ebf4cc0936484a585de
Ben-Edwards44 <benjedwards007@icloud.com>
2023-09-19 19:02:01 +0100

    Added num text

--- random_walk.py
+++ random_walk.py
@@ -2,13 +2,14 @@ import pygame
 import num_generate
 import math
 
+
+#Set to True to see the lines as they are drawn
 SHOW_LINE_DRAWS = False
+SHOW_TEXT = False
 
 NUM_ITERS = 5
 LENGTH = 3.4
 COLOURS = ((38, 70, 83), (42, 157, 143), (233, 196, 106), (244, 162, 97), (231, 111, 81))
-#COLOURS = ((3, 4, 94), (2, 62, 138), (0, 119, 182), (0, 150, 199), (0, 180, 216), (72, 202, 228), (144, 224, 239), (173, 232, 244), (202, 240, 248))
-#COLOURS = ((239, 71, 111), (255, 209, 102), (6, 214, 160), (17, 138, 178), (7, 59, 76))
 
 START_X = 400
 START_Y = 100
@@ -42,9 +43,9 @@ def draw_line(x1, y1, x2, y2, colour_inx):
 
 
 def nums_text(num):
-    text_surf = font.render(f"Digits: {num}", False, (255, 255, 255), (0, 0, 0))
+    text_surf = font.render(f"Number of lines: {num}", False, (255, 255, 255), (0, 0, 0))
     text_rect = text_surf.get_rect()
-    text_rect.center = (50, 50)
+    text_rect.topleft = (50, 50)
 
     window.blit(text_surf, text_rect)
 
@@ -53,7 +54,7 @@ def complete_walk(nums):
     prev_x = START_X
     prev_y = START_Y
 
-    for i in nums:
+    for x, i in enumerate(nums):
         angle = int(i) % 360
         new_x, new_y = walk(prev_x, prev_y, angle)
 
@@ -61,6 +62,12 @@ def complete_walk(nums):
 
         prev_x = new_x
         prev_y = new_y
+
+        if SHOW_LINE_DRAWS and SHOW_TEXT:
+            nums_text(x + 1)
+
+    if SHOW_TEXT:
+        nums_text(len(nums))
 
     pygame.display.update()
 
